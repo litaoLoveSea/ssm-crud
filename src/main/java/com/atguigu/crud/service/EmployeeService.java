b595e52266dad432b05ac40a197c541ca1a0638e
litaoLoveSea <fengzhenggaogao@163.com>
2020-06-09 22:14:23 +0800

    2

--- src/main/java/com/atguigu/crud/service/EmployeeService.java
+++ src/main/java/com/atguigu/crud/service/EmployeeService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.atguigu.crud.bean.Employee;
+import com.atguigu.crud.bean.EmployeeExample;
+import com.atguigu.crud.bean.EmployeeExample.Criteria;
 import com.atguigu.crud.dao.EmployeeMapper;
 
 @Service
@@ -20,6 +22,14 @@ public class EmployeeService {
 
 	public void save(Employee employee) {
 		employeeMapper.insertSelective(employee);
+	}
+
+	public boolean checkUser(String empName) {
+		EmployeeExample employeeExample = new EmployeeExample();
+		Criteria createCriteria = employeeExample.createCriteria();
+		createCriteria.andEmpNameEqualTo(empName);
+		long count = employeeMapper.countByExample(employeeExample);
+		return count == 0 ;
 	}
 
 	
